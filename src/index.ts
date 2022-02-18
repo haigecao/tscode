b44c4c341f7520bf725dc1f634d2d40467123250
haigecao <haigecao@tencent.com>
2022-02-18 22:56:51 +0800

    feat: add stash

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./type/datatype";
 import "./type/enum";
 import "./type/interface";
+import "./type/function";
 
 let hello: string = "Hello haigecao";
 document.querySelectorAll(".app")[0].innerHTML = hello;
